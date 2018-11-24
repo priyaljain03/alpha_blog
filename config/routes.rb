88b4ba8c7cd7c69a283fad174f61336b5f370f5a
priyal jain <jainpriyal03@gmail.com>
2018-11-24 02:46:08 +0000

    create homepage

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-root 'pages#home'
-get 'about',to: 'pages#about'
+
+root 'articles#home'
+get 'articles/about',to: 'articles#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
