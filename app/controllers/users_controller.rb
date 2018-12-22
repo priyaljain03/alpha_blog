a68e4c07012d3a246df1f26c9782cead05897b6e
priyal jain <jainpriyal03@gmail.com>
2018-12-22 10:34:26 +0000

    brypt gem added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,18 @@ class UsersController<ApplicationController
     end
     
     def create
-        debugger
+        @user=User.new(user_params)
+        if @user.save
+            flash[:success]="Welcome to JustBlogIt"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
     end
     
+    private
+    def user_params
+        params.require(:user).permit(:username,:email,:password)
+    end
+      
 end
